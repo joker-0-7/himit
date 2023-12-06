5dc24f1364a4536d6691a9e222f26149ac58ef06
joker-0-7 <93847410+joker-0-7@users.noreply.github.com>
2023-12-06 02:20:33 +0200

    Delete src/app/notifications directory

--- src/app/notifications/page.jsx
+++
@@ -1,8 +0,0 @@
-// const Notifications = () => {
-//   return (
-//     <div className="notifications">
-//       <h1>Notifications</h1>
-//     </div>
-//   );
-// };
-// export default Notifications;
