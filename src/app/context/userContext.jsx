c1ec48a91e6dca4c32cfd77914213dec21ec1e27
joker-0-7 <93847410+joker-0-7@users.noreply.github.com>
2024-03-10 05:17:19 +0200

    Update userContext.jsx

--- src/app/context/userContext.jsx
+++ src/app/context/userContext.jsx
@@ -2,20 +2,25 @@
 import { useState, useEffect, createContext } from "react";
 import axios from "axios";
 import { useRouter } from "next/navigation";
+import Login from "../login/page";
 
 const UserContext = createContext();
 
 const UseProvider = ({ children }) => {
-  const [state, setState] = useState({ user: {}, token: "" });
-  const router = useRouter();
-  useEffect(() => {
-    setState(JSON.parse(window.localStorage.getItem("auth")));
-  }, []);
-
-  return (
-    <UserContext.Provider value={[state, setState]}>
-      {children}
-    </UserContext.Provider>
-  );
+    const [state, setState] = useState({ user: {}, token: "" });
+    const router = useRouter();
+    useEffect(() => {
+        setState(JSON.parse(window.localStorage.getItem("auth")));
+    }, []);
+    useEffect(() => {
+        if (!state) {
+            router.push("/login");
+        }
+    }, [state]);
+    return (
+        <UserContext.Provider value={[state, setState]}>
+            {children}
+        </UserContext.Provider>
+    );
 };
 export { UserContext, UseProvider };
