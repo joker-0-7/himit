3ed004deb56294971d72d028629757772a4b59ef
joker-0-7 <93847410+joker-0-7@users.noreply.github.com>
2024-04-22 01:03:13 +0200

    Update page.jsx

--- src/app/class-schedules/page.jsx
+++ src/app/class-schedules/page.jsx
@@ -55,9 +55,9 @@ function ClassSchedules() {
         router.push(`/class-schedules/edit/${e}`);
     };
     return (
-        <div>
+        <div className="row">
             <Nav />
-            <div className="container">
+            <div className="container col-10 px-5">
                 <div className="row">
                     <div className="col-12">
                         <Header />
